b10ffebbe0e26bc5f83e8009fb7b3402a0f28bdf
Gamatoza <gamatext@yandex.com>
2019-12-11 11:15:27 +0300

    datepicker has been added

--- blocks/addUser.php
+++ blocks/addUser.php
@@ -13,22 +13,29 @@
         <!--Добавить проверку на выбор не пустой строки -->
         <label for="NIS"><b>Name Initial Surname</b></label>
         <input type="text" placeholder="Enter NIS" name="NIS" class="form-control" required>
-        
+
         <label for="Birthday"><b>Birthday</b></label>
-        <input class="datepicker form-control" id="datepicker"> <!--PLACE DATEPICKER THERE-->
+        <input class="datepicker form-control" id="datepicker">
         <script>
-            $(document).ready(function(){
-            $('#datepicker').click(function(){
-                $(this).datepicker($.datepicker.regional["ru"]); 
-            });
-            
+            $(document).ready(function() {
+                $('#datepicker').datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    dateFormat: 'dd/mm/yy',
+                    defaultDate: '-20y -1m -247d',
+                    yearRange: '-50:+0'
+                });
+                $("#birthdate").focus(function() {
+                    $(this).datepicker("show")
+                });
+
             });
         </script>
 
         <label for="psw"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" class="form-control" required>
 
-        <button type="submit" class="btn btn-success mt-3" >Registrate</button>
+        <button type="submit" class="btn btn-success mt-3">Registrate</button>
     </div>
-    
+
 </form>
