e6aa254592dc124c561a8cc1f53b578338ae2e97
Gamatoza <gamatext@yandex.com>
2019-12-11 11:28:41 +0300

    finally i fixed it, checkmate git

--- registrate-new-employee.php
+++ registrate-new-employee.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="js/jquery-ui-1.12.1/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
     <title>Новый работник</title>
